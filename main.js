1e96c7cc8f1560a2b72a45d3ff2dea3d724f8d15
Feihong Hsu <feihong.hsu@gmail.com>
2019-01-25 22:46:23 -0600

    Fix main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const Server = require('./src/Server.bs.js')
+const Events = require('./src/Events.bs.js')
 const Event_bs = require('./src/gen/event_bs.bs.js')
 
 async function main() {
-  let output = await Server.fetch()
+  let output = await Events.fetch()
   // console.log(output)
   let jsOutput = Event_bs.write_eventOutput(output)
   console.log(jsOutput)
